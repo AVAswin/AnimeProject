133550591d784f05dbd3e705b39e363a00d416d1
A V ASWIN <88997856+AVAswin@users.noreply.github.com>
2022-12-19 23:25:56 +0530

    Add files via upload

---
+++ action.js
@@ -0,0 +1,19 @@
+
+
+
+$("p").on("mouseover",function() {
+    $(".card-body").slideDown();
+    
+  
+});
+
+
+$("img").on("mouseout",function() {
+    $("#card-expand").css("visibility","hidden");
+    
+});
+
+
+
+
+    
